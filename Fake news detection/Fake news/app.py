99901878322da89736db72ba3ab58e91b2753db6
SHIVAM PAL <75245714+I-am-ShivamPal@users.noreply.github.com>
2025-09-20 13:39:11 +0530

    Update app.py

--- Fake news detection/Fake news/app.py
+++ Fake news detection/Fake news/app.py
@@ -14,7 +14,8 @@ from sklearn.metrics import accuracy_score
 nltk.download('stopwords')
 
 # Load data
-news_df = pd.read_csv('https://drive.google.com/uc?id=1ip8TLiWnvbcbhsUMeVS3NnQPMGrqolJD')
+# news_df = pd.read_csv('https://drive.google.com/uc?id=1ip8TLiWnvbcbhsUMeVS3NnQPMGrqolJD')
+news_df = pd.read_csv('https://drive.google.com/uc?id=1ip8TLiWnvbcbhsUMeVS3NnQPMGrqolJD', low_memory=False)
 news_df = news_df.fillna(' ')
 news_df['content'] = news_df['author'] + ' ' + news_df['title']
 
@@ -66,5 +67,6 @@ if input_text:
         st.error('🚨 The News is Fake!')
     else:
         st.success('✅ The News is Real!')
+
 
 
